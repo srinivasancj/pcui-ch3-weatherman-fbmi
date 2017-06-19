379c53c70cb19389b7b06c304aa84ccbc42a0203
Srini Janarthanam <srinivasancj@gmail.com>
2017-06-19 17:49:00 +0100

    fix

--- index.js
+++ index.js
@@ -71,6 +71,7 @@ app.post('/webhook/', function (req, res) {
 					console.log(body)
 					body = body.substring(1,body.length-1);
                     body = body.replace(/\\/g, '')
+                    body = body.replace(/u0027/g, '\'');
 	                let botOut = JSON.parse(body)
 	
 	                if (botOut.utterance != null){     
