b151403d5ecc567dd8b3eea20a95add145ff6324
Srini Janarthanam <srinivasancj@gmail.com>
2017-06-19 15:53:49 +0100

    removing processText

--- index.js
+++ index.js
@@ -88,8 +88,9 @@ app.post('/webhook/', function (req, res) {
 })
 
 function sendTextMessage(sender, text) {
-    let textToDisplay = processText(botOut.utterance);
-	if (textToDisplay != 'null'){
+    //let textToDisplay = processText(botOut.utterance);
+	let textToDisplay = botOut.utterance;
+    if (textToDisplay != 'null'){
         let messageData = { 'text':textToDisplay }
         request({
             url: 'https://graph.facebook.com/v2.6/me/messages',
